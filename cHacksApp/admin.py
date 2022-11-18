5591ebf9fadaf8595ddb6b5a94b4cdcc8f4bcc77
proflamyt <53262578+proflamyt@users.noreply.github.com>
2022-11-18 13:24:13 +0100

    adding app

---
+++ cHacksApp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import User, Questions, Mark
+
+# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Questions)
+admin.site.register(Mark)

5fdb14db759a02f780b69977dcea359ff28f5bd0
proflamyt <53262578+proflamyt@users.noreply.github.com>
2022-12-09 21:44:12 +0100

    added

--- cHacksApp/urls.py
+++ cHacksApp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import AnswerQuestions, QuestionsView, UserViewSet
+from .views import AnswerQuestions, Certificate, QuestionsView, UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -13,5 +13,6 @@ router.register(r'questions', QuestionsView)
 urlpatterns = [
     path('', include(router.urls)),
     path('answers/<int:pk>', AnswerQuestions.as_view(), name='answers'),
+    path('certificate/', Certificate.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]

70a513b7be173eaadcedc7aa30bcbf4f6d0fa260
proflamyt <53262578+proflamyt@users.noreply.github.com>
2023-02-07 13:57:37 +0100

    user admin

--- cHacksApp/admin.py
+++ cHacksApp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import User, Questions, Mark, School, SchoolScore, Notification
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Questions)
 admin.site.register(Mark)
 admin.site.register(School)

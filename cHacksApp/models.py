20225e6098f6ea701a5805031734c0aa222ab58a
proflamyt <53262578+proflamyt@users.noreply.github.com>
2023-03-09 08:33:26 +0100

    str to model

--- cHacksApp/models.py
+++ cHacksApp/models.py
@@ -113,6 +113,9 @@ class Questions(models.Model):
     file = models.FileField(blank=True)
     code = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.description[:10]
+
 
 class Notification(models.Model):
    # title = models.CharField(max_length=255)
@@ -127,6 +130,9 @@ class Mark(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='marked')
     answered = models.BooleanField(default=False)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.user
 
 
 @receiver (pre_save, sender=Mark)
